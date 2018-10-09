1bd69fadc46eb46720d784de7de279a4a1246213
Yohan Jabin <yohan.jabin@epitech.eu>
2018-10-09 11:32:52 +0200

    fix merge

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 module.exports = (app) => {
 	app.get('/', (req, res) => {
-		if (!req.isAuthenticated())
+		if (!req.isAuthenticated()) {
 			res.redirect('/login');
-		res.render('pages/index', {
-			error: req.flash('error'),
-			isConnected: true
-		});
+		}
+		else {
+			res.render('pages/index', {
+				error: req.flash('error'),
+				isConnected: true
+			});
+		}
 	});
 }
