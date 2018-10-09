c57a99c847d8359c0edf229a5f0fa3582b04e10c
antony.leron <antony.leron@epitech.eu>
2018-10-09 12:31:55 +0200

    correct page index

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 module.exports = (app) => {
 	app.get('/', (req, res) => {
-		if (!req.isAuthenticated()) {
-			res.redirect('/login');
-		}
-		else {
+		// if (!req.isAuthenticated()) {
+		// 	res.redirect('/login');
+		// }
+		// else {
 			res.render('pages/index', {
 				error: req.flash('error'),
 				isConnected: true,
 				tabSelected: 'home'
 			});
-		}
+		// }
 	});
 }
