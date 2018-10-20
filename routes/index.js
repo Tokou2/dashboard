35ad80077a8a3d9930103b36677335dac934a364
antony.leron <antony.leron@epitech.eu>
2018-10-20 16:40:00 +0200

    correct css

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 module.exports = (app) => {
 	app.get('/', (req, res) => {
-		// if (!req.isAuthenticated()) {
-		// 	res.redirect('/login');
-		// }
-		// else {
+		if (!req.isAuthenticated()) {
+			res.redirect('/login');
+		}
+		else {
 			res.render('pages/index', {
 				error: req.flash('error'),
 				isConnected: true,
 				tabSelected: 'home',
 				widgets : {
 					"meteo": {
-						"name": "zizi"
-					}
+						"name": "Météo",
+					},
 					"bourse": {
 						"name": "Bourse"
-					}
+					},
 					"cinema": {
 						"name": "Cinema"
 					}
 				}
 			});
-		// }
+		}
 	});
 }
