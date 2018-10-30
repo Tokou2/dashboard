bed00d308ba623a6c00e563b3c0e93efa7d64eaf
Yohan Jabin <yohan.jabin@epitech.eu>
2018-10-30 13:32:13 +0100

    fix options

--- services/weather/weather.js
+++ services/weather/weather.js
@@ -13,26 +13,7 @@ class WeatherService extends Service
 			widgets[i].set('base_url', 'http://dataservice.accuweather.com');
 		}
 		super('weather', widgets);
-		//this.apiKey = '77Ti1sS5wZ2bv1IBmYz052Gxl251cAGi';
-		//this.baseUrl = `http://dataservice.accuweather.com`;
 	}
-
-	/*searchCity(city) {
-		return new Promise((resolve, reject) => {
-			superagent.get(`${this.baseUrl}/locations/v1/cities/search`).query({
-				apikey: this.apiKey,
-				q: city
-			}).end((err, res) => {
-				if (err)
-					reject(err);
-				resolve(res.body);
-			});
-		});
-	}
-
-	setCity(city) {
-
-	}*/
 }
 
 module.exports = WeatherService;
